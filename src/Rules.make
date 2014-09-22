RobertCNelson/ti-sdk-pvr

sgx: import of 4.10.00.01 GPL kernel modules
Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

Graphics_SDK/Rules.make
@@ -21,7 +21,7 @@ KERNEL_INSTALL_DIR=$(LINUXKERNEL_INSTALL_DIR)
 TARGETFS_INSTALL_DIR=$(DESTDIR)
 
 # Set installation folder
-GRAPHICS_INSTALL_DIR=$(HOME)/Graphics_SDK_4_09_00_01
+GRAPHICS_INSTALL_DIR=$(HOME)/Graphics_SDK_4_10_00_01
 ############### All the fields below are OPTIONAL ############################
 ############### BEST SUGGESTED TO LEAVE THE BELOW AS IS ############################
 

rcn-ee/ti-sgx

src/Rules.make
@@ -21,7 +21,7 @@ KERNEL_INSTALL_DIR=$(LINUXKERNEL_INSTALL_DIR)
 TARGETFS_INSTALL_DIR=$(DESTDIR)
 
 # Set installation folder
-GRAPHICS_INSTALL_DIR=$(HOME)/src_4_09_00_01
+GRAPHICS_INSTALL_DIR=$(HOME)/src_4_10_00_01
 ############### All the fields below are OPTIONAL ############################
 ############### BEST SUGGESTED TO LEAVE THE BELOW AS IS ############################
 