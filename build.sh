a3569618c537ffe4cb6a649c6c483d5310812efb
Robert Nelson <robertcnelson@gmail.com>
2014-09-22 16:11:43 -0500

    add build.sh script
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+x86_dir="/opt/github/ti-linux-kernel-dev"
+x86_compiler="gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin/arm-linux-gnueabihf-"
+
+mkdir -p ./gfx_rel_es8.x || true
+if [ "x${ARCH}" = "xarmv7l" ] ; then
+	make_options="GRAPHICS_INSTALL_DIR=./ KERNEL_INSTALL_DIR=/build/buildd/linux-src CSTOOL_DIR=/usr/ CSTOOL_PREFIX= "
+else
+	make_options="GRAPHICS_INSTALL_DIR=./ KERNEL_INSTALL_DIR=${x86_dir}/KERNEL CSTOOL_DIR=${x86_dir}/dl/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux CSTOOL_PREFIX=arm-linux-gnueabihf-"
+fi
+
+make ARCH=arm ${make_options} clean
+make ARCH=arm ${make_options} BUILD=release OMAPES=8.x FBDEV=no all
+#
