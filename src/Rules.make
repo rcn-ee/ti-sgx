RobertCNelson/ti-sdk-pvr

sgx: import of 5.01.00.01 GPL kernel modules
Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

Graphics_SDK/Rules.make
@@ -21,7 +21,7 @@ KERNEL_INSTALL_DIR=$(LINUXKERNEL_INSTALL_DIR)
 TARGETFS_INSTALL_DIR=$(DESTDIR)
 
 # Set installation folder
-GRAPHICS_INSTALL_DIR=$(HOME)/Graphics_SDK_4_10_00_01
+GRAPHICS_INSTALL_DIR=$(HOME)/Graphics_SDK_5_01_00_01
 ############### All the fields below are OPTIONAL ############################
 ############### BEST SUGGESTED TO LEAVE THE BELOW AS IS ############################
 
@@ -72,6 +72,10 @@ GFX_DBG_ES6_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_dbg_es6.x
 GFX_REL_ES8_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_rel_es8.x
 
 GFX_DBG_ES8_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_dbg_es8.x
+
+GFX_REL_ES9_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_rel_es9.x
+
+GFX_DBG_ES9_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_dbg_es9.x
 
 GFX_OGLES_REL_LIB_DESTPATH=$(GFX_OGLES_SDKPATH)/Builds/OGLES/LinuxARMV7
 

rcn-ee/ti-sgx

src/Rules.make
@@ -21,7 +21,7 @@ KERNEL_INSTALL_DIR=$(LINUXKERNEL_INSTALL_DIR)
 TARGETFS_INSTALL_DIR=$(DESTDIR)
 
 # Set installation folder
-GRAPHICS_INSTALL_DIR=$(HOME)/src_4_10_00_01
+GRAPHICS_INSTALL_DIR=$(HOME)/src_5_01_00_01
 ############### All the fields below are OPTIONAL ############################
 ############### BEST SUGGESTED TO LEAVE THE BELOW AS IS ############################
 
@@ -72,6 +72,10 @@ GFX_DBG_ES6_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_dbg_es6.x
 GFX_REL_ES8_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_rel_es8.x
 
 GFX_DBG_ES8_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_dbg_es8.x
+
+GFX_REL_ES9_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_rel_es9.x
+
+GFX_DBG_ES9_LIB_SRCPATH=$(GRAPHICS_INSTALL_DIR)/gfx_dbg_es9.x
 
 GFX_OGLES_REL_LIB_DESTPATH=$(GFX_OGLES_SDKPATH)/Builds/OGLES/LinuxARMV7
 