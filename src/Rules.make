RobertCNelson/ti-sdk-pvr

sgx: import of 4.08.00.02 GPL kernel modules
Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

Graphics_SDK/Rules.make
@@ -39,7 +39,7 @@ export EXTRA_LFLAGS
 ######################## NOT MODIFIABLE BY USER ###############################
 
 # Set installation folder
-GRAPHICS_INSTALL_DIR=$(HOME)/Graphics_SDK_4_08_00_01
+GRAPHICS_INSTALL_DIR=$(HOME)/Graphics_SDK_4_08_00_02
 
 # Location of demo executables and install information
 GFX_DEMOS_EXECPATH=$(GRAPHICS_INSTALL_DIR)/gfxsdkdemos

rcn-ee/ti-sgx

src/Rules.make
@@ -39,7 +39,7 @@ export EXTRA_LFLAGS
 ######################## NOT MODIFIABLE BY USER ###############################
 
 # Set installation folder
-GRAPHICS_INSTALL_DIR=$(HOME)/src_4_08_00_01
+GRAPHICS_INSTALL_DIR=$(HOME)/src_4_08_00_02
 
 # Location of demo executables and install information
 GFX_DEMOS_EXECPATH=$(GRAPHICS_INSTALL_DIR)/gfxsdkdemos