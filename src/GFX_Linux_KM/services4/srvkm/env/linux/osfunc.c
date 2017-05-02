199ee6b8f89a35703367658958054d58d9de82ce
Robert Nelson <robertcnelson@gmail.com>
2017-05-02 13:11:58 -0500

    sgx: fix for 4.11.x
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- src/GFX_Linux_KM/services4/srvkm/env/linux/osfunc.c
+++ src/GFX_Linux_KM/services4/srvkm/env/linux/osfunc.c
@@ -3423,7 +3423,11 @@ PVRSRV_ERROR OSReleasePhysPageAddr(IMG_HANDLE hOSWrapMem)
                         SetPageDirty(psPage);
                     }
 	        }
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0)
+                put_page(psPage);
+#else
                 page_cache_release(psPage);
+#endif
 	    }
             break;
         }
@@ -3605,7 +3609,11 @@ PVRSRV_ERROR OSAcquirePhysPageAddr(IMG_VOID *pvCPUVAddr,
     bMMapSemHeld = IMG_TRUE;
 
     /* Get page list */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0)
+    psInfo->iNumPagesMapped = get_user_pages_remote(current, current->mm, uStartAddr, psInfo->iNumPages, FOLL_WRITE, psInfo->ppsPages, NULL, NULL);
+#else
     psInfo->iNumPagesMapped = get_user_pages(current, current->mm, uStartAddr, psInfo->iNumPages, 1, 0, psInfo->ppsPages, NULL);
+#endif
 
     if (psInfo->iNumPagesMapped >= 0)
     {
